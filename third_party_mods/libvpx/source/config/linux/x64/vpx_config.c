49cfb241388c14e7540e2d396e1eb911919e9c83
ajm@google.com <ajm@google.com@4adac7df-926f-26a2-2b94-8c16560cd09d>
2011-07-09 21:16:30 +0000

    Reorganize libvpx to mimic Chromium's layout. We now sync libvpx to a subdirectory of third_party/libvpx where we've submitted our few wrapper files. This avoids having to "svn export" to the directory and allows direct committing of files. Review URL: http://webrtc-codereview.appspot.com/64001
    git-svn-id: http://webrtc.googlecode.com/svn/trunk@186 4adac7df-926f-26a2-2b94-8c16560cd09d

--- third_party_mods/libvpx/source/config/linux/x64/vpx_config.c
+++
@@ -1,2 +0,0 @@
-static const char* const cfg = "--target=x86_64-linux-gcc --enable-pic --disable-install-docs --disable-install-srcs --disable-examples --disable-psnr";
-const char *vpx_codec_build_config(void) {return cfg;}
