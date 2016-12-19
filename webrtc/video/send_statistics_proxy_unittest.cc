zyjiang08/webrtc_m94

commit bdbfd04d3b61d70b4ea7c504f6de05c1341d7503

Move usage of QualityScaler to ViEEncoder.
This brings QualityScaler much more in line with OveruseFrameDetector.
The two classes are conceptually similar, and should be used in the
same way. The biggest changes in this CL are:
- Quality scaling is now only done in ViEEncoder and not in each
  encoder implementation separately.
- QualityScaler now checks the average QP asynchronously, instead of
  having to be polled on each frame.
- QualityScaler is no longer responsible for actually scaling the frames,
  but has a callback to ViEEncoder that it uses to express it's desire
  for lower resolution.

BUG=webrtc:6495

Review-Url: https://codereview.webrtc.org/2398963003
Cr-Commit-Position: refs/heads/master@{#15286}

webrtc/modules/video_coding/codecs/vp8/simulcast_encoder_adapter.cc
@@ -451,10 +451,6 @@ bool SimulcastEncoderAdapter::Initialized() const {
   return !streaminfos_.empty();
 }
 
-void SimulcastEncoderAdapter::OnDroppedFrame() {
-  streaminfos_[0].encoder->OnDroppedFrame();
-}
-
 bool SimulcastEncoderAdapter::SupportsNativeHandle() const {
   // We should not be calling this method before streaminfos_ are configured.
   RTC_DCHECK(!streaminfos_.empty());
@@ -463,6 +459,14 @@ bool SimulcastEncoderAdapter::SupportsNativeHandle() const {
       return false;
   }
   return true;
+}
+
+VideoEncoder::ScalingSettings SimulcastEncoderAdapter::GetScalingSettings()
+    const {
+  // Turn off quality scaling for simulcast.
+  if (NumberOfStreams(codec_) != 1)
+    return VideoEncoder::ScalingSettings(false);
+  return streaminfos_[0].encoder->GetScalingSettings();
 }
 
 const char* SimulcastEncoderAdapter::ImplementationName() const {

webrtc/video/send_statistics_proxy.cc
@@ -505,9 +505,8 @@ void SendStatisticsProxy::OnSendEncodedImage(
 
   uma_container_->key_frame_counter_.Add(encoded_image._frameType ==
                                          kVideoFrameKey);
-
   stats_.bw_limited_resolution =
-      encoded_image.adapt_reason_.quality_resolution_downscales > 0 ||
+      stats_.bw_limited_resolution ||
       encoded_image.adapt_reason_.bw_resolutions_disabled > 0;
 
   if (encoded_image.adapt_reason_.quality_resolution_downscales != -1) {
@@ -588,10 +587,11 @@ void SendStatisticsProxy::OnIncomingFrame(int width, int height) {
   uma_container_->cpu_limited_frame_counter_.Add(stats_.cpu_limited_resolution);
 }
 
-void SendStatisticsProxy::SetCpuRestrictedResolution(
-    bool cpu_restricted_resolution) {
+void SendStatisticsProxy::SetResolutionRestrictionStats(bool bandwidth,
+                                                        bool cpu) {
   rtc::CritScope lock(&crit_);
-  stats_.cpu_limited_resolution = cpu_restricted_resolution;
+  stats_.bw_limited_resolution = bandwidth;
+  stats_.cpu_limited_resolution = cpu;
 }
 
 void SendStatisticsProxy::OnCpuRestrictedResolutionChanged(
@@ -599,6 +599,13 @@ void SendStatisticsProxy::OnCpuRestrictedResolutionChanged(
   rtc::CritScope lock(&crit_);
   stats_.cpu_limited_resolution = cpu_restricted_resolution;
   ++stats_.number_of_cpu_adapt_changes;
+}
+
+void SendStatisticsProxy::OnQualityRestrictedResolutionChanged(
+    bool restricted) {
+  rtc::CritScope lock(&crit_);
+  uma_container_->quality_downscales_counter_.Add(restricted);
+  stats_.bw_limited_resolution = restricted;
 }
 
 void SendStatisticsProxy::RtcpPacketTypesCounterUpdated(

webrtc/video/send_statistics_proxy.h
@@ -57,12 +57,9 @@ class SendStatisticsProxy : public CpuOveruseMetricsObserver,
   // Used to update incoming frame rate.
   void OnIncomingFrame(int width, int height);
 
-  // Used to indicate that the current input frame resolution is restricted due
-  // to cpu usage.
-  void SetCpuRestrictedResolution(bool cpu_restricted);
-  // Used to update the number of times the input frame resolution has changed
-  // due to cpu adaptation.
   void OnCpuRestrictedResolutionChanged(bool cpu_restricted_resolution);
+  void OnQualityRestrictedResolutionChanged(bool restricted);
+  void SetResolutionRestrictionStats(bool bandwidth, bool cpu);
 
   void OnEncoderStatsUpdate(uint32_t framerate, uint32_t bitrate);
   void OnSuspendChange(bool is_suspended);

webrtc/video/vie_encoder.cc
@@ -257,9 +257,6 @@ ViEEncoder::ViEEncoder(uint32_t number_of_cores,
       has_received_rpsi_(false),
       picture_id_rpsi_(0),
       clock_(Clock::GetRealTimeClock()),
-      degradation_preference_(
-          VideoSendStream::DegradationPreference::kBalanced),
-      cpu_restricted_counter_(0),
       last_frame_width_(0),
       last_frame_height_(0),
       last_captured_timestamp_(0),
@@ -292,6 +289,7 @@ void ViEEncoder::Stop() {
     rate_allocator_.reset();
     video_sender_.RegisterExternalEncoder(nullptr, settings_.payload_type,
                                           false);
+    quality_scaler_ = nullptr;
     shutdown_event_.Set();
   });
 
@@ -318,17 +316,12 @@ void ViEEncoder::SetSource(
   source_proxy_->SetSource(source, degradation_preference);
   encoder_queue_.PostTask([this, degradation_preference] {
     RTC_DCHECK_RUN_ON(&encoder_queue_);
-    degradation_preference_ = degradation_preference;
-    // Set the stats for if we are currently CPU restricted. We are CPU
-    // restricted depending on degradation preference and
-    // if the overusedetector has currently detected overuse which is counted in
-    // |cpu_restricted_counter_|
-    // We do this on the encoder task queue to avoid a race with the stats set
-    // in ViEEncoder::NormalUsage and ViEEncoder::OveruseDetected.
-    stats_proxy_->SetCpuRestrictedResolution(
-        degradation_preference_ !=
-            VideoSendStream::DegradationPreference::kMaintainResolution &&
-        cpu_restricted_counter_ != 0);
+    scaling_enabled_ =
+        (degradation_preference !=
+         VideoSendStream::DegradationPreference::kMaintainResolution);
+    stats_proxy_->SetResolutionRestrictionStats(
+        scaling_enabled_ && scale_counter_[kQuality] > 0,
+        scaling_enabled_ && scale_counter_[kCpu] > 0);
   });
 }
 
@@ -420,6 +413,18 @@ void ViEEncoder::ReconfigureEncoder() {
 
   sink_->OnEncoderConfigurationChanged(
       std::move(streams), encoder_config_.min_transmit_bitrate_bps);
+
+  const auto scaling_settings = settings_.encoder->GetScalingSettings();
+  if (scaling_settings.enabled && scaling_enabled_) {
+    if (scaling_settings.thresholds) {
+      quality_scaler_.reset(
+          new QualityScaler(this, *(scaling_settings.thresholds)));
+    } else {
+      quality_scaler_.reset(new QualityScaler(this, codec_type_));
+    }
+  } else {
+    quality_scaler_.reset(nullptr);
+  }
 }
 
 void ViEEncoder::OnFrame(const VideoFrame& video_frame) {
@@ -497,6 +502,7 @@ void ViEEncoder::TraceFrameDropEnd() {
 void ViEEncoder::EncodeVideoFrame(const VideoFrame& video_frame,
                                   int64_t time_when_posted_in_ms) {
   RTC_DCHECK_RUN_ON(&encoder_queue_);
+
   if (pre_encode_callback_)
     pre_encode_callback_->OnFrame(video_frame);
 
@@ -574,13 +580,23 @@ EncodedImageCallback::Result ViEEncoder::OnEncodedImage(
 
   int64_t time_sent = clock_->TimeInMilliseconds();
   uint32_t timestamp = encoded_image._timeStamp;
-
-  encoder_queue_.PostTask([this, timestamp, time_sent] {
+  const int qp = encoded_image.qp_;
+  encoder_queue_.PostTask([this, timestamp, time_sent, qp] {
     RTC_DCHECK_RUN_ON(&encoder_queue_);
     overuse_detector_.FrameSent(timestamp, time_sent);
+    if (quality_scaler_)
+      quality_scaler_->ReportQP(qp);
   });
 
   return result;
+}
+
+void ViEEncoder::OnDroppedFrame() {
+  encoder_queue_.PostTask([this] {
+    RTC_DCHECK_RUN_ON(&encoder_queue_);
+    if (quality_scaler_)
+      quality_scaler_->ReportDroppedFrame();
+  });
 }
 
 void ViEEncoder::SendStatistics(uint32_t bit_rate, uint32_t frame_rate) {
@@ -654,47 +670,67 @@ void ViEEncoder::OnBitrateUpdated(uint32_t bitrate_bps,
   }
 }
 
-void ViEEncoder::OveruseDetected() {
+void ViEEncoder::ScaleDown(ScaleReason reason) {
   RTC_DCHECK_RUN_ON(&encoder_queue_);
-  if (degradation_preference_ ==
-          VideoSendStream::DegradationPreference::kMaintainResolution ||
-      cpu_restricted_counter_ >= kMaxCpuDowngrades) {
+  if (!scaling_enabled_)
     return;
-  }
-  LOG(LS_INFO) << "CPU overuse detected. Requesting lower resolution.";
   // Request lower resolution if the current resolution is lower than last time
   // we asked for the resolution to be lowered.
-  // Update stats accordingly.
   int current_pixel_count = last_frame_height_ * last_frame_width_;
-  if (!max_pixel_count_ || current_pixel_count < *max_pixel_count_) {
-    max_pixel_count_ = rtc::Optional<int>(current_pixel_count);
-    max_pixel_count_step_up_ = rtc::Optional<int>();
-    stats_proxy_->OnCpuRestrictedResolutionChanged(true);
-    ++cpu_restricted_counter_;
-    source_proxy_->RequestResolutionLowerThan(current_pixel_count);
+  if (max_pixel_count_ && current_pixel_count >= *max_pixel_count_)
+    return;
+  switch (reason) {
+    case kQuality:
+      if (scale_counter_[reason] >= kMaxQualityDowngrades)
+        return;
+      stats_proxy_->OnQualityRestrictedResolutionChanged(true);
+      break;
+    case kCpu:
+      if (scale_counter_[reason] >= kMaxCpuDowngrades)
+        return;
+      // Update stats accordingly.
+      stats_proxy_->OnCpuRestrictedResolutionChanged(true);
+      break;
+  }
+  max_pixel_count_ = rtc::Optional<int>(current_pixel_count);
+  max_pixel_count_step_up_ = rtc::Optional<int>();
+  ++scale_counter_[reason];
+  source_proxy_->RequestResolutionLowerThan(current_pixel_count);
+  LOG(LS_INFO) << "Scaling down resolution.";
+  for (size_t i = 0; i < kScaleReasonSize; ++i) {
+    LOG(LS_INFO) << "Scaled " << scale_counter_[i]
+                 << " times for reason: " << (i ? "quality" : "cpu");
   }
 }
 
-void ViEEncoder::NormalUsage() {
+void ViEEncoder::ScaleUp(ScaleReason reason) {
   RTC_DCHECK_RUN_ON(&encoder_queue_);
-  if (degradation_preference_ ==
-          VideoSendStream::DegradationPreference::kMaintainResolution ||
-      cpu_restricted_counter_ == 0) {
+  if (scale_counter_[reason] == 0 || !scaling_enabled_)
     return;
-  }
-
-  LOG(LS_INFO) << "CPU underuse detected. Requesting higher resolution.";
+  // Only scale if resolution is higher than last time
+  // we requested higher resolution.
   int current_pixel_count = last_frame_height_ * last_frame_width_;
-  // Request higher resolution if we are CPU restricted and the the current
-  // resolution is higher than last time we requested higher resolution.
-  // Update stats accordingly.
-  if (!max_pixel_count_step_up_ ||
-      current_pixel_count > *max_pixel_count_step_up_) {
-    max_pixel_count_ = rtc::Optional<int>();
-    max_pixel_count_step_up_ = rtc::Optional<int>(current_pixel_count);
-    --cpu_restricted_counter_;
-    stats_proxy_->OnCpuRestrictedResolutionChanged(cpu_restricted_counter_ > 0);
-    source_proxy_->RequestHigherResolutionThan(current_pixel_count);
+  if (current_pixel_count <= max_pixel_count_step_up_.value_or(0))
+    return;
+  switch (reason) {
+    case kQuality:
+      stats_proxy_->OnQualityRestrictedResolutionChanged(
+          scale_counter_[reason] > 1);
+      break;
+    case kCpu:
+      // Update stats accordingly.
+      stats_proxy_->OnCpuRestrictedResolutionChanged(scale_counter_[reason] >
+                                                     1);
+      break;
+  }
+  max_pixel_count_ = rtc::Optional<int>();
+  max_pixel_count_step_up_ = rtc::Optional<int>(current_pixel_count);
+  --scale_counter_[reason];
+  source_proxy_->RequestHigherResolutionThan(current_pixel_count);
+  LOG(LS_INFO) << "Scaling up resolution.";
+  for (size_t i = 0; i < kScaleReasonSize; ++i) {
+    LOG(LS_INFO) << "Scaled " << scale_counter_[i]
+                 << " times for reason: " << (i ? "quality" : "cpu");
   }
 }
 

commit 812d5cf91a3cb25ab6bdd2e676ad6ecc21cd4d55

Reland of Properly report number of quality downscales in stats. (patchset #1 id:1 of https://codereview.webrtc.org/2586783003/ )
Reason for revert:
Bug affecting perf tests has been fixed. The issue was that I had accidentally disabled cpu overuse adaptation based on the encoders ScalingSettings, not just quality-based scaling.

Original issue's description:
> Revert of Properly report number of quality downscales in stats. (patchset #11 id:220001 of https://codereview.webrtc.org/2564373002/ )
>
> Reason for revert:
> Breaks perf tests
>
> Original issue's description:
> > Properly report number of quality downscales in stats.
> >
> > A regression was introduced in bdbfd04 that caused these stats to
> > be reported incorrectly. This used to be only implemented for VP8
> > but should now be available for all codecs.
> >
> > BUG=webrtc:6860
> >
> > Review-Url: https://codereview.webrtc.org/2564373002
> > Cr-Commit-Position: refs/heads/master@{#15673}
> > Committed: https://chromium.googlesource.com/external/webrtc/+/8ef52df93fb131b577a8177d3eb03b9cfdf1416a
>
> TBR=asapersson@webrtc.org,stefan@webrtc.org
> # Skipping CQ checks because original CL landed less than 1 days ago.
> NOPRESUBMIT=true
> NOTREECHECKS=true
> NOTRY=true
> BUG=webrtc:6860
>
> Review-Url: https://codereview.webrtc.org/2586783003
> Cr-Commit-Position: refs/heads/master@{#15678}
> Committed: https://chromium.googlesource.com/external/webrtc/+/255d8e67bbb27ae07031c144e562c99bdf0bceec

TBR=asapersson@webrtc.org,stefan@webrtc.org
# Skipping CQ checks because original CL landed less than 1 days ago.
NOPRESUBMIT=true
NOTREECHECKS=true
NOTRY=true
BUG=webrtc:6860

Review-Url: https://codereview.webrtc.org/2588743002
Cr-Commit-Position: refs/heads/master@{#15680}

webrtc/modules/video_coding/codecs/vp8/simulcast_encoder_adapter.cc
@@ -466,7 +466,7 @@ bool SimulcastEncoderAdapter::SupportsNativeHandle() const {
 VideoEncoder::ScalingSettings SimulcastEncoderAdapter::GetScalingSettings()
     const {
   // Turn off quality scaling for simulcast.
-  if (NumberOfStreams(codec_) != 1)
+  if (!Initialized() || NumberOfStreams(codec_) != 1)
     return VideoEncoder::ScalingSettings(false);
   return streaminfos_[0].encoder->GetScalingSettings();
 }

webrtc/video/send_statistics_proxy.cc
@@ -506,17 +506,13 @@ void SendStatisticsProxy::OnSendEncodedImage(
   uma_container_->key_frame_counter_.Add(encoded_image._frameType ==
                                          kVideoFrameKey);
   stats_.bw_limited_resolution =
-      stats_.bw_limited_resolution ||
-      encoded_image.adapt_reason_.bw_resolutions_disabled > 0;
+      encoded_image.adapt_reason_.bw_resolutions_disabled > 0 ||
+      quality_downscales_ > 0;
 
-  if (encoded_image.adapt_reason_.quality_resolution_downscales != -1) {
-    bool downscaled =
-        encoded_image.adapt_reason_.quality_resolution_downscales > 0;
-    uma_container_->quality_limited_frame_counter_.Add(downscaled);
-    if (downscaled) {
-      uma_container_->quality_downscales_counter_.Add(
-          encoded_image.adapt_reason_.quality_resolution_downscales);
-    }
+  if (quality_downscales_ != -1) {
+    uma_container_->quality_limited_frame_counter_.Add(quality_downscales_ > 0);
+    if (quality_downscales_ > 0)
+      uma_container_->quality_downscales_counter_.Add(quality_downscales_);
   }
   if (encoded_image.adapt_reason_.bw_resolutions_disabled != -1) {
     bool bw_limited = encoded_image.adapt_reason_.bw_resolutions_disabled > 0;
@@ -587,11 +583,20 @@ void SendStatisticsProxy::OnIncomingFrame(int width, int height) {
   uma_container_->cpu_limited_frame_counter_.Add(stats_.cpu_limited_resolution);
 }
 
-void SendStatisticsProxy::SetResolutionRestrictionStats(bool bandwidth,
-                                                        bool cpu) {
+void SendStatisticsProxy::SetResolutionRestrictionStats(
+    bool scaling_enabled,
+    bool cpu_restricted,
+    int num_quality_downscales) {
   rtc::CritScope lock(&crit_);
-  stats_.bw_limited_resolution = bandwidth;
-  stats_.cpu_limited_resolution = cpu;
+  if (scaling_enabled) {
+    quality_downscales_ = num_quality_downscales;
+    stats_.bw_limited_resolution = quality_downscales_ > 0;
+    stats_.cpu_limited_resolution = cpu_restricted;
+  } else {
+    stats_.bw_limited_resolution = false;
+    stats_.cpu_limited_resolution = false;
+    quality_downscales_ = -1;
+  }
 }
 
 void SendStatisticsProxy::OnCpuRestrictedResolutionChanged(
@@ -602,10 +607,10 @@ void SendStatisticsProxy::OnCpuRestrictedResolutionChanged(
 }
 
 void SendStatisticsProxy::OnQualityRestrictedResolutionChanged(
-    bool restricted) {
+    int num_quality_downscales) {
   rtc::CritScope lock(&crit_);
-  uma_container_->quality_downscales_counter_.Add(restricted);
-  stats_.bw_limited_resolution = restricted;
+  quality_downscales_ = num_quality_downscales;
+  stats_.bw_limited_resolution = quality_downscales_ > 0;
 }
 
 void SendStatisticsProxy::RtcpPacketTypesCounterUpdated(

webrtc/video/send_statistics_proxy.h
@@ -58,8 +58,10 @@ class SendStatisticsProxy : public CpuOveruseMetricsObserver,
   void OnIncomingFrame(int width, int height);
 
   void OnCpuRestrictedResolutionChanged(bool cpu_restricted_resolution);
-  void OnQualityRestrictedResolutionChanged(bool restricted);
-  void SetResolutionRestrictionStats(bool bandwidth, bool cpu);
+  void OnQualityRestrictedResolutionChanged(int num_quality_downscales);
+  void SetResolutionRestrictionStats(bool scaling_enabled,
+                                     bool cpu_restricted,
+                                     int num_quality_downscales);
 
   void OnEncoderStatsUpdate(uint32_t framerate, uint32_t bitrate);
   void OnSuspendChange(bool is_suspended);
@@ -154,6 +156,7 @@ class SendStatisticsProxy : public CpuOveruseMetricsObserver,
   uint32_t last_sent_frame_timestamp_ GUARDED_BY(crit_);
   std::map<uint32_t, StatsUpdateTimes> update_times_ GUARDED_BY(crit_);
   rtc::ExpFilter encode_time_ GUARDED_BY(crit_);
+  int quality_downscales_ GUARDED_BY(crit_) = 0;
 
   // Contains stats used for UMA histograms. These stats will be reset if
   // content type changes between real-time video and screenshare, since these

webrtc/video/vie_encoder.cc
@@ -15,6 +15,7 @@
 #include <utility>
 
 #include "webrtc/modules/video_coding/include/video_codec_initializer.h"
+#include "webrtc/base/arraysize.h"
 #include "webrtc/base/checks.h"
 #include "webrtc/base/logging.h"
 #include "webrtc/base/trace_event.h"
@@ -268,6 +269,7 @@ ViEEncoder::ViEEncoder(uint32_t number_of_cores,
       has_received_rpsi_(false),
       picture_id_rpsi_(0),
       clock_(Clock::GetRealTimeClock()),
+      scale_counter_(kScaleReasonSize, 0),
       last_frame_width_(0),
       last_frame_height_(0),
       last_captured_timestamp_(0),
@@ -339,12 +341,10 @@ void ViEEncoder::SetSource(
   source_proxy_->SetSource(source, degradation_preference);
   encoder_queue_.PostTask([this, degradation_preference] {
     RTC_DCHECK_RUN_ON(&encoder_queue_);
-    scaling_enabled_ =
-        (degradation_preference !=
+    scaling_enabled_ = (degradation_preference !=
          VideoSendStream::DegradationPreference::kMaintainResolution);
     stats_proxy_->SetResolutionRestrictionStats(
-        scaling_enabled_ && scale_counter_[kQuality] > 0,
-        scaling_enabled_ && scale_counter_[kCpu] > 0);
+        scaling_enabled_, scale_counter_[kCpu] > 0, scale_counter_[kQuality]);
   });
 }
 
@@ -440,7 +440,7 @@ void ViEEncoder::ReconfigureEncoder() {
       std::move(streams), encoder_config_.min_transmit_bitrate_bps);
 
   const auto scaling_settings = settings_.encoder->GetScalingSettings();
-  if (scaling_settings.enabled && scaling_enabled_) {
+  if (scaling_enabled_ && scaling_settings.enabled) {
     if (scaling_settings.thresholds) {
       quality_scaler_.reset(
           new QualityScaler(this, *(scaling_settings.thresholds)));
@@ -449,6 +449,8 @@ void ViEEncoder::ReconfigureEncoder() {
     }
   } else {
     quality_scaler_.reset(nullptr);
+    stats_proxy_->SetResolutionRestrictionStats(
+        false, scale_counter_[kCpu] > 0, scale_counter_[kQuality]);
   }
 }
 
@@ -596,9 +598,8 @@ EncodedImageCallback::Result ViEEncoder::OnEncodedImage(
   // Encoded is called on whatever thread the real encoder implementation run
   // on. In the case of hardware encoders, there might be several encoders
   // running in parallel on different threads.
-  if (stats_proxy_) {
+  if (stats_proxy_)
     stats_proxy_->OnSendEncodedImage(encoded_image, codec_specific_info);
-  }
 
   EncodedImageCallback::Result result =
       sink_->OnEncodedImage(encoded_image, codec_specific_info, fragmentation);
@@ -707,7 +708,8 @@ void ViEEncoder::ScaleDown(ScaleReason reason) {
     return;
   switch (reason) {
     case kQuality:
-      stats_proxy_->OnQualityRestrictedResolutionChanged(true);
+      stats_proxy_->OnQualityRestrictedResolutionChanged(
+          scale_counter_[reason] + 1);
       break;
     case kCpu:
       if (scale_counter_[reason] >= kMaxCpuDowngrades)
@@ -739,7 +741,7 @@ void ViEEncoder::ScaleUp(ScaleReason reason) {
   switch (reason) {
     case kQuality:
       stats_proxy_->OnQualityRestrictedResolutionChanged(
-          scale_counter_[reason] > 1);
+          scale_counter_[reason] - 1);
       break;
     case kCpu:
       // Update stats accordingly.

webrtc/video/vie_encoder.h
@@ -211,7 +211,7 @@ class ViEEncoder : public rtc::VideoSinkInterface<VideoFrame>,
   Clock* const clock_;
   // Counters used for deciding if the video resolution is currently
   // restricted, and if so, why.
-  int scale_counter_[kScaleReasonSize] ACCESS_ON(&encoder_queue_) = {0};
+  std::vector<int> scale_counter_ ACCESS_ON(&encoder_queue_);
   // Set depending on degradation preferences
   bool scaling_enabled_ ACCESS_ON(&encoder_queue_) = false;
 

webrtc/video_frame.h
@@ -136,14 +136,10 @@ class EncodedImage {
   EncodedImage(uint8_t* buffer, size_t length, size_t size)
       : _buffer(buffer), _length(length), _size(size) {}
 
+  // TODO(kthelgason): get rid of this struct as it only has a single member
+  // remaining.
   struct AdaptReason {
-    AdaptReason()
-        : quality_resolution_downscales(-1),
-          bw_resolutions_disabled(-1) {}
-
-    int quality_resolution_downscales;  // Number of times this frame is down
-                                        // scaled in resolution due to quality.
-                                        // Or -1 if information is not provided.
+    AdaptReason() : bw_resolutions_disabled(-1) {}
     int bw_resolutions_disabled;  // Number of resolutions that are not sent
                                   // due to bandwidth for this frame.
                                   // Or -1 if information is not provided.

webrtc/video/send_statistics_proxy_unittest.cc
@@ -31,6 +31,12 @@ const int kWidth = 640;
 const int kHeight = 480;
 const int kQpIdx0 = 21;
 const int kQpIdx1 = 39;
+const CodecSpecificInfo kDefaultCodecInfo = []() {
+  CodecSpecificInfo codec_info;
+  codec_info.codecType = kVideoCodecVP8;
+  codec_info.codecSpecific.VP8.simulcastIdx = 0;
+  return codec_info;
+}();
 }  // namespace
 
 class SendStatisticsProxyTest : public ::testing::Test {
@@ -659,10 +665,10 @@ TEST_F(SendStatisticsProxyTest,
 TEST_F(SendStatisticsProxyTest,
        QualityLimitedHistogramsNotUpdatedWhenDisabled) {
   EncodedImage encoded_image;
-  // encoded_image.adapt_reason_.quality_resolution_downscales disabled by
-  // default: -1
+  statistics_proxy_->SetResolutionRestrictionStats(false /* scaling_enabled */,
+                                                   0, 0);
   for (int i = 0; i < SendStatisticsProxy::kMinRequiredMetricsSamples; ++i)
-    statistics_proxy_->OnSendEncodedImage(encoded_image, nullptr);
+    statistics_proxy_->OnSendEncodedImage(encoded_image, &kDefaultCodecInfo);
 
   // Histograms are updated when the statistics_proxy_ is deleted.
   statistics_proxy_.reset();
@@ -674,11 +680,9 @@ TEST_F(SendStatisticsProxyTest,
 
 TEST_F(SendStatisticsProxyTest,
        QualityLimitedHistogramsUpdatedWhenEnabled_NoResolutionDownscale) {
-  const int kDownscales = 0;
   EncodedImage encoded_image;
-  encoded_image.adapt_reason_.quality_resolution_downscales = kDownscales;
   for (int i = 0; i < SendStatisticsProxy::kMinRequiredMetricsSamples; ++i)
-    statistics_proxy_->OnSendEncodedImage(encoded_image, nullptr);
+    statistics_proxy_->OnSendEncodedImage(encoded_image, &kDefaultCodecInfo);
 
   // Histograms are updated when the statistics_proxy_ is deleted.
   statistics_proxy_.reset();
@@ -695,10 +699,9 @@ TEST_F(SendStatisticsProxyTest,
        QualityLimitedHistogramsUpdatedWhenEnabled_TwoResolutionDownscales) {
   const int kDownscales = 2;
   EncodedImage encoded_image;
-  encoded_image.adapt_reason_.quality_resolution_downscales = kDownscales;
+  statistics_proxy_->OnQualityRestrictedResolutionChanged(kDownscales);
   for (int i = 0; i < SendStatisticsProxy::kMinRequiredMetricsSamples; ++i)
-    statistics_proxy_->OnSendEncodedImage(encoded_image, nullptr);
-
+    statistics_proxy_->OnSendEncodedImage(encoded_image, &kDefaultCodecInfo);
   // Histograms are updated when the statistics_proxy_ is deleted.
   statistics_proxy_.reset();
   EXPECT_EQ(
@@ -720,12 +723,18 @@ TEST_F(SendStatisticsProxyTest, GetStatsReportsBandwidthLimitedResolution) {
   EncodedImage encoded_image;
   statistics_proxy_->OnSendEncodedImage(encoded_image, nullptr);
   EXPECT_FALSE(statistics_proxy_->GetStats().bw_limited_resolution);
-  // Resolution not scaled.
+
+  // Simulcast disabled resolutions
+  encoded_image.adapt_reason_.bw_resolutions_disabled = 1;
+  statistics_proxy_->OnSendEncodedImage(encoded_image, nullptr);
+  EXPECT_TRUE(statistics_proxy_->GetStats().bw_limited_resolution);
+
   encoded_image.adapt_reason_.bw_resolutions_disabled = 0;
   statistics_proxy_->OnSendEncodedImage(encoded_image, nullptr);
   EXPECT_FALSE(statistics_proxy_->GetStats().bw_limited_resolution);
-  // Resolution scaled due to bandwidth.
-  encoded_image.adapt_reason_.bw_resolutions_disabled = 1;
+
+  // Resolution scaled due to quality.
+  statistics_proxy_->OnQualityRestrictedResolutionChanged(1);
   statistics_proxy_->OnSendEncodedImage(encoded_image, nullptr);
   EXPECT_TRUE(statistics_proxy_->GetStats().bw_limited_resolution);
 }