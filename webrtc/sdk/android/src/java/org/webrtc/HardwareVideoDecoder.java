zyjiang08/webrtc_m94

commit 0712b849e67e07bf9e45582f2b5361217ea8b4be

Create VideoDecoderFactory interface and implementation.
The implementation creates an Android hardware video decoder.  It is built
around the same patterns as the HardwareVideoEncoderFactory.

This change pulls some shared code and constants into a common "utils" class.

Finally, adds an instrumentation test for the HardwareVideoDecoder.

BUG=webrtc:7760

Change-Id: Iea6eaae7727925743cb54f7c3153a6c07d62f55d
Reviewed-on: https://chromium-review.googlesource.com/536254
Commit-Queue: Bjorn Mellem <mellem@webrtc.org>
Reviewed-by: Sami Kalliomäki <sakal@webrtc.org>
Reviewed-by: Peter Thatcher <pthatcher@webrtc.org>
Cr-Commit-Position: refs/heads/master@{#18686}

webrtc/sdk/android/src/java/org/webrtc/HardwareVideoDecoder.java
@@ -38,13 +38,6 @@ class HardwareVideoDecoder implements VideoDecoder {
   private static final String MEDIA_FORMAT_KEY_CROP_TOP = "crop-top";
   private static final String MEDIA_FORMAT_KEY_CROP_BOTTOM = "crop-bottom";
 
-  // NV12 color format supported by QCOM codec, but not declared in MediaCodec -
-  // see /hardware/qcom/media/mm-core/inc/OMX_QCOMExtns.h
-  private static final int COLOR_QCOM_FORMATYVU420PackedSemiPlanar32m4ka = 0x7FA30C01;
-  private static final int COLOR_QCOM_FORMATYVU420PackedSemiPlanar16m4ka = 0x7FA30C02;
-  private static final int COLOR_QCOM_FORMATYVU420PackedSemiPlanar64x32Tile2m8ka = 0x7FA30C03;
-  private static final int COLOR_QCOM_FORMATYUV420PackedSemiPlanar32m = 0x7FA30C04;
-
   // MediaCodec.release() occasionally hangs.  Release stops waiting and reports failure after
   // this timeout.
   private static final int MEDIA_CODEC_RELEASE_TIMEOUT_MS = 5000;
@@ -132,7 +125,7 @@ class HardwareVideoDecoder implements VideoDecoder {
     try {
       codec = MediaCodec.createByCodecName(codecName);
     } catch (IOException | IllegalArgumentException e) {
-      Logging.e(TAG, "Cannot create media decoder");
+      Logging.e(TAG, "Cannot create media decoder " + codecName);
       return VideoCodecStatus.ERROR;
     }
     try {
@@ -168,8 +161,8 @@ class HardwareVideoDecoder implements VideoDecoder {
 
     int size = frame.buffer.remaining();
     if (size == 0) {
-      Logging.e(TAG, "decode() - input buffer empty");
-      return VideoCodecStatus.ERR_PARAMETER;
+      Logging.e(TAG, "decode() - no input data");
+      return VideoCodecStatus.ERROR;
     }
 
     // Load dimensions from shared memory under the dimension lock.
@@ -215,13 +208,13 @@ class HardwareVideoDecoder implements VideoDecoder {
       index = codec.dequeueInputBuffer(0 /* timeout */);
     } catch (IllegalStateException e) {
       Logging.e(TAG, "dequeueInputBuffer failed", e);
-      return VideoCodecStatus.ERROR;
+      return VideoCodecStatus.FALLBACK_SOFTWARE;
     }
     if (index < 0) {
       // Decoder is falling behind.  No input buffers available.
       // The decoder can't simply drop frames; it might lose a key frame.
       Logging.e(TAG, "decode() - no HW buffers available; decoder falling behind");
-      return VideoCodecStatus.ERROR;
+      return VideoCodecStatus.FALLBACK_SOFTWARE;
     }
 
     ByteBuffer buffer;
@@ -229,12 +222,12 @@ class HardwareVideoDecoder implements VideoDecoder {
       buffer = codec.getInputBuffers()[index];
     } catch (IllegalStateException e) {
       Logging.e(TAG, "getInputBuffers failed", e);
-      return VideoCodecStatus.ERROR;
+      return VideoCodecStatus.FALLBACK_SOFTWARE;
     }
 
     if (buffer.capacity() < size) {
       Logging.e(TAG, "decode() - HW buffer too small");
-      return VideoCodecStatus.ERROR;
+      return VideoCodecStatus.FALLBACK_SOFTWARE;
     }
     buffer.put(frame.buffer);
 
@@ -245,7 +238,7 @@ class HardwareVideoDecoder implements VideoDecoder {
     } catch (IllegalStateException e) {
       Logging.e(TAG, "queueInputBuffer failed", e);
       decodeStartTimes.pollLast();
-      return VideoCodecStatus.ERROR;
+      return VideoCodecStatus.FALLBACK_SOFTWARE;
     }
     if (keyFrameRequired) {
       keyFrameRequired = false;
@@ -472,18 +465,12 @@ class HardwareVideoDecoder implements VideoDecoder {
   }
 
   private boolean isSupportedColorFormat(int colorFormat) {
-    switch (colorFormat) {
-      case CodecCapabilities.COLOR_FormatYUV420Planar:
-      case CodecCapabilities.COLOR_FormatYUV420SemiPlanar:
-      case CodecCapabilities.COLOR_QCOM_FormatYUV420SemiPlanar:
-      case COLOR_QCOM_FORMATYVU420PackedSemiPlanar32m4ka:
-      case COLOR_QCOM_FORMATYVU420PackedSemiPlanar16m4ka:
-      case COLOR_QCOM_FORMATYVU420PackedSemiPlanar64x32Tile2m8ka:
-      case COLOR_QCOM_FORMATYUV420PackedSemiPlanar32m:
+    for (int supported : MediaCodecUtils.DECODER_COLOR_FORMATS) {
+      if (supported == colorFormat) {
         return true;
-      default:
-        return false;
+      }
     }
+    return false;
   }
 
   private static void copyI420(ByteBuffer src, int offset, VideoFrame.I420Buffer frameBuffer,
@@ -505,15 +492,14 @@ class HardwareVideoDecoder implements VideoDecoder {
     copyPlane(src, vPos, uvStride, frameBuffer.getDataV(), 0, frameBuffer.getStrideV(), chromaWidth,
         chromaHeight);
 
-    // If the sliceHeight is odd, duplicate the last rows of chroma.  Copy the last row of the U and
-    // V channels and append them at the end of each channel.
+    // If the sliceHeight is odd, duplicate the last rows of chroma.
     if (sliceHeight % 2 != 0) {
       int strideU = frameBuffer.getStrideU();
-      int endU = chromaHeight * strideU;
-      copyRow(frameBuffer.getDataU(), endU - strideU, frameBuffer.getDataU(), endU, chromaWidth);
+      uPos = chromaHeight * strideU;
+      copyRow(frameBuffer.getDataU(), uPos - strideU, frameBuffer.getDataU(), uPos, chromaWidth);
       int strideV = frameBuffer.getStrideV();
-      int endV = chromaHeight * strideV;
-      copyRow(frameBuffer.getDataV(), endV - strideV, frameBuffer.getDataV(), endV, chromaWidth);
+      vPos = chromaHeight * strideV;
+      copyRow(frameBuffer.getDataV(), vPos - strideV, frameBuffer.getDataV(), vPos, chromaWidth);
     }
   }
 

webrtc/sdk/android/instrumentationtests/src/org/webrtc/HardwareVideoDecoderTest.java
@@ -1,0 +1,125 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package org.webrtc;
+
+import static org.junit.Assert.assertEquals;
+
+import android.annotation.TargetApi;
+import android.graphics.Matrix;
+import android.support.test.filters.MediumTest;
+import android.util.Log;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.CountDownLatch;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/** Unit tests for {@link HardwareVideoDecoder}. */
+@TargetApi(16)
+@RunWith(BaseJUnit4ClassRunner.class)
+public final class HardwareVideoDecoderTest {
+  private static final String TAG = "HardwareVideoDecoderTest";
+
+  private static final boolean ENABLE_INTEL_VP8_ENCODER = true;
+  private static final boolean ENABLE_H264_HIGH_PROFILE = true;
+  private static final VideoDecoder.Settings SETTINGS =
+      new VideoDecoder.Settings(1 /* core */, 640 /* width */, 480 /* height */);
+
+  @Test
+  @MediumTest
+  public void testInitialize() {
+    HardwareVideoEncoderFactory encoderFactory =
+        new HardwareVideoEncoderFactory(ENABLE_INTEL_VP8_ENCODER, ENABLE_H264_HIGH_PROFILE);
+    VideoCodecInfo[] supportedCodecs = encoderFactory.getSupportedCodecs();
+    if (supportedCodecs.length == 0) {
+      Log.i(TAG, "No hardware encoding support, skipping testInitialize");
+      return;
+    }
+
+    HardwareVideoDecoderFactory decoderFactory = new HardwareVideoDecoderFactory();
+
+    VideoDecoder decoder = decoderFactory.createDecoder(supportedCodecs[0].name);
+    assertEquals(decoder.initDecode(SETTINGS, null), VideoCodecStatus.OK);
+    assertEquals(decoder.release(), VideoCodecStatus.OK);
+  }
+
+  @Test
+  @MediumTest
+  public void testDecode() throws InterruptedException {
+    HardwareVideoEncoderFactory encoderFactory =
+        new HardwareVideoEncoderFactory(ENABLE_INTEL_VP8_ENCODER, ENABLE_H264_HIGH_PROFILE);
+    VideoCodecInfo[] supportedCodecs = encoderFactory.getSupportedCodecs();
+    if (supportedCodecs.length == 0) {
+      Log.i(TAG, "No hardware encoding support, skipping testEncodeYuvBuffer");
+      return;
+    }
+
+    // Set up the decoder.
+    HardwareVideoDecoderFactory decoderFactory = new HardwareVideoDecoderFactory();
+    VideoDecoder decoder = decoderFactory.createDecoder(supportedCodecs[0].name);
+
+    final long presentationTimestampUs = 20000;
+
+    final CountDownLatch decodeDone = new CountDownLatch(1);
+    final AtomicReference<VideoFrame> decoded = new AtomicReference<>();
+    VideoDecoder.Callback decodeCallback = new VideoDecoder.Callback() {
+      @Override
+      public void onDecodedFrame(VideoFrame frame, Integer decodeTimeMs, Integer qp) {
+        decoded.set(frame);
+        decodeDone.countDown();
+      }
+    };
+    assertEquals(decoder.initDecode(SETTINGS, decodeCallback), VideoCodecStatus.OK);
+
+    // Set up an encoder to produce a valid encoded frame.
+    VideoEncoder encoder = encoderFactory.createEncoder(supportedCodecs[0]);
+    final CountDownLatch encodeDone = new CountDownLatch(1);
+    final AtomicReference<EncodedImage> encoded = new AtomicReference<>();
+    VideoEncoder.Callback encodeCallback = new VideoEncoder.Callback() {
+      @Override
+      public void onEncodedFrame(EncodedImage image, VideoEncoder.CodecSpecificInfo info) {
+        encoded.set(image);
+        encodeDone.countDown();
+      }
+    };
+    assertEquals(
+        encoder.initEncode(
+            new VideoEncoder.Settings(1, SETTINGS.width, SETTINGS.height, 300, 30), encodeCallback),
+        VideoCodecStatus.OK);
+
+    // First, encode a frame.
+    VideoFrame.I420Buffer buffer = new I420BufferImpl(SETTINGS.width, SETTINGS.height);
+    VideoFrame frame =
+        new VideoFrame(buffer, 0 /* rotation */, presentationTimestampUs * 1000, new Matrix());
+    VideoEncoder.EncodeInfo info = new VideoEncoder.EncodeInfo(
+        new EncodedImage.FrameType[] {EncodedImage.FrameType.VideoFrameKey});
+
+    assertEquals(encoder.encode(frame, info), VideoCodecStatus.OK);
+
+    ThreadUtils.awaitUninterruptibly(encodeDone);
+
+    // Now decode the frame.
+    assertEquals(
+        decoder.decode(encoded.get(), new VideoDecoder.DecodeInfo(false, 0)), VideoCodecStatus.OK);
+
+    ThreadUtils.awaitUninterruptibly(decodeDone);
+
+    frame = decoded.get();
+    assertEquals(frame.getRotation(), 0);
+    assertEquals(frame.getTimestampNs(), presentationTimestampUs * 1000);
+    assertEquals(frame.getTransformMatrix(), new Matrix());
+    assertEquals(frame.getBuffer().getWidth(), SETTINGS.width);
+    assertEquals(frame.getBuffer().getHeight(), SETTINGS.height);
+
+    assertEquals(decoder.release(), VideoCodecStatus.OK);
+    assertEquals(encoder.release(), VideoCodecStatus.OK);
+  }
+}

webrtc/sdk/android/BUILD.gn
@@ -341,6 +341,7 @@ android_library("libjingle_peerconnection_java") {
     "api/org/webrtc/GlShader.java",
     "api/org/webrtc/GlTextureFrameBuffer.java",
     "api/org/webrtc/GlUtil.java",
+    "api/org/webrtc/HardwareVideoDecoderFactory.java",
     "api/org/webrtc/HardwareVideoEncoderFactory.java",
     "api/org/webrtc/IceCandidate.java",
     "api/org/webrtc/MediaCodecVideoDecoder.java",
@@ -371,6 +372,7 @@ android_library("libjingle_peerconnection_java") {
     "api/org/webrtc/VideoCodecInfo.java",
     "api/org/webrtc/VideoCodecStatus.java",
     "api/org/webrtc/VideoDecoder.java",
+    "api/org/webrtc/VideoDecoderFactory.java",
     "api/org/webrtc/VideoEncoder.java",
     "api/org/webrtc/VideoEncoderFactory.java",
     "api/org/webrtc/VideoFileRenderer.java",
@@ -393,6 +395,7 @@ android_library("libjingle_peerconnection_java") {
     "src/java/org/webrtc/HardwareVideoEncoder.java",
     "src/java/org/webrtc/Histogram.java",
     "src/java/org/webrtc/I420BufferImpl.java",
+    "src/java/org/webrtc/MediaCodecUtils.java",
     "src/java/org/webrtc/VideoCodecType.java",
     "src/java/org/webrtc/WrappedNativeI420Buffer.java",
     "src/java/org/webrtc/YuvConverter.java",
@@ -426,6 +429,7 @@ if (rtc_include_tests) {
       "instrumentationtests/src/org/webrtc/FileVideoCapturerTest.java",
       "instrumentationtests/src/org/webrtc/GlRectDrawerTest.java",
       "instrumentationtests/src/org/webrtc/HardwareVideoEncoderTest.java",
+      "instrumentationtests/src/org/webrtc/HardwareVideoDecoderTest.java",
       "instrumentationtests/src/org/webrtc/MediaCodecVideoEncoderTest.java",
       "instrumentationtests/src/org/webrtc/NetworkMonitorTest.java",
       "instrumentationtests/src/org/webrtc/PeerConnectionTest.java",

webrtc/sdk/android/api/org/webrtc/HardwareVideoDecoderFactory.java
@@ -1,0 +1,100 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+package org.webrtc;
+
+import static org.webrtc.MediaCodecUtils.EXYNOS_PREFIX;
+import static org.webrtc.MediaCodecUtils.INTEL_PREFIX;
+import static org.webrtc.MediaCodecUtils.NVIDIA_PREFIX;
+import static org.webrtc.MediaCodecUtils.QCOM_PREFIX;
+
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaCodecInfo.CodecCapabilities;
+import android.media.MediaCodecList;
+import android.os.Build;
+
+/** Factory for Android hardware VideoDecoders. */
+@SuppressWarnings("deprecation") // API level 16 requires use of deprecated methods.
+public class HardwareVideoDecoderFactory implements VideoDecoderFactory {
+  private static final String TAG = "HardwareVideoDecoderFactory";
+
+  @Override
+  public VideoDecoder createDecoder(String codecType) {
+    VideoCodecType type = VideoCodecType.valueOf(codecType);
+    MediaCodecInfo info = findCodecForType(type);
+
+    if (info == null) {
+      return null; // No support for this codec type.
+    }
+
+    CodecCapabilities capabilities = info.getCapabilitiesForType(type.mimeType());
+    return new HardwareVideoDecoder(info.getName(), type,
+        MediaCodecUtils.selectColorFormat(MediaCodecUtils.DECODER_COLOR_FORMATS, capabilities));
+  }
+
+  private MediaCodecInfo findCodecForType(VideoCodecType type) {
+    // HW decoding is not supported on builds before KITKAT.
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+      return null;
+    }
+
+    for (int i = 0; i < MediaCodecList.getCodecCount(); ++i) {
+      MediaCodecInfo info = null;
+      try {
+        info = MediaCodecList.getCodecInfoAt(i);
+      } catch (IllegalArgumentException e) {
+        Logging.e(TAG, "Cannot retrieve encoder codec info", e);
+      }
+
+      if (info == null || info.isEncoder()) {
+        continue;
+      }
+
+      if (isSupportedCodec(info, type)) {
+        return info;
+      }
+    }
+    return null; // No support for this type.
+  }
+
+  // Returns true if the given MediaCodecInfo indicates a supported encoder for the given type.
+  private boolean isSupportedCodec(MediaCodecInfo info, VideoCodecType type) {
+    if (!MediaCodecUtils.codecSupportsType(info, type)) {
+      return false;
+    }
+    // Check for a supported color format.
+    if (MediaCodecUtils.selectColorFormat(
+            MediaCodecUtils.DECODER_COLOR_FORMATS, info.getCapabilitiesForType(type.mimeType()))
+        == null) {
+      return false;
+    }
+    return isHardwareSupported(info, type);
+  }
+
+  private boolean isHardwareSupported(MediaCodecInfo info, VideoCodecType type) {
+    String name = info.getName();
+    switch (type) {
+      case VP8:
+        // QCOM, Intel, Exynos, and Nvidia all supported for VP8.
+        return name.startsWith(QCOM_PREFIX) || name.startsWith(INTEL_PREFIX)
+            || name.startsWith(EXYNOS_PREFIX) || name.startsWith(NVIDIA_PREFIX);
+      case VP9:
+        // QCOM and Exynos supported for VP9.
+        return name.startsWith(QCOM_PREFIX) || name.startsWith(EXYNOS_PREFIX);
+      case H264:
+        // QCOM, Intel, and Exynos supported for H264.
+        return name.startsWith(QCOM_PREFIX) || name.startsWith(INTEL_PREFIX)
+            || name.startsWith(EXYNOS_PREFIX);
+      default:
+        return false;
+    }
+  }
+}

commit a8cc05613da87ea770b5b1c144f66e5d86614bcc

Fix some bugs in the HardwareVideoDecoder.
This change preserves rotation through the decoder, rather than requiring
callers to keep track of rotation.  The test now uses a non-zero rotation
to ensure it is preserved.

Commit 0712b84 inadvertently reverted several changes that weren't merged
properly before submit.  In particular, it clobbered some log messages,
comments, and error codes.  This change fixes those mistakes.

BUG=webrtc:7760

Change-Id: If529ee59fc56de7937e362dc15591295e2cf9f79
Reviewed-on: https://chromium-review.googlesource.com/546415
Commit-Queue: Sami Kalliomäki <sakal@webrtc.org>
Reviewed-by: Peter Thatcher <pthatcher@webrtc.org>
Reviewed-by: Sami Kalliomäki <sakal@webrtc.org>
Cr-Commit-Position: refs/heads/master@{#18752}

webrtc/sdk/android/src/java/org/webrtc/HardwareVideoDecoder.java
@@ -50,7 +50,18 @@ class HardwareVideoDecoder implements VideoDecoder {
 
   private final String codecName;
   private final VideoCodecType codecType;
-  private final Deque<Long> decodeStartTimes;
+
+  private static class FrameInfo {
+    final long decodeStartTimeMs;
+    final int rotation;
+
+    FrameInfo(long decodeStartTimeMs, int rotation) {
+      this.decodeStartTimeMs = decodeStartTimeMs;
+      this.rotation = rotation;
+    }
+  }
+
+  private final Deque<FrameInfo> frameInfos;
   private int colorFormat;
 
   // Output thread runs a loop which polls MediaCodec for decoded output buffers.  It reformats
@@ -95,7 +106,7 @@ class HardwareVideoDecoder implements VideoDecoder {
     this.codecName = codecName;
     this.codecType = codecType;
     this.colorFormat = colorFormat;
-    this.decodeStartTimes = new LinkedBlockingDeque<>();
+    this.frameInfos = new LinkedBlockingDeque<>();
   }
 
   @Override
@@ -161,8 +172,8 @@ class HardwareVideoDecoder implements VideoDecoder {
 
     int size = frame.buffer.remaining();
     if (size == 0) {
-      Logging.e(TAG, "decode() - no input data");
-      return VideoCodecStatus.ERROR;
+      Logging.e(TAG, "decode() - input buffer empty");
+      return VideoCodecStatus.ERR_PARAMETER;
     }
 
     // Load dimensions from shared memory under the dimension lock.
@@ -177,7 +188,7 @@ class HardwareVideoDecoder implements VideoDecoder {
         && (frame.encodedWidth != width || frame.encodedHeight != height)) {
       VideoCodecStatus status = reinitDecode(frame.encodedWidth, frame.encodedHeight);
       if (status != VideoCodecStatus.OK) {
-        return VideoCodecStatus.FALLBACK_SOFTWARE;
+        return status;
       }
     }
 
@@ -208,13 +219,13 @@ class HardwareVideoDecoder implements VideoDecoder {
       index = codec.dequeueInputBuffer(0 /* timeout */);
     } catch (IllegalStateException e) {
       Logging.e(TAG, "dequeueInputBuffer failed", e);
-      return VideoCodecStatus.FALLBACK_SOFTWARE;
+      return VideoCodecStatus.ERROR;
     }
     if (index < 0) {
       // Decoder is falling behind.  No input buffers available.
       // The decoder can't simply drop frames; it might lose a key frame.
       Logging.e(TAG, "decode() - no HW buffers available; decoder falling behind");
-      return VideoCodecStatus.FALLBACK_SOFTWARE;
+      return VideoCodecStatus.ERROR;
     }
 
     ByteBuffer buffer;
@@ -222,23 +233,23 @@ class HardwareVideoDecoder implements VideoDecoder {
       buffer = codec.getInputBuffers()[index];
     } catch (IllegalStateException e) {
       Logging.e(TAG, "getInputBuffers failed", e);
-      return VideoCodecStatus.FALLBACK_SOFTWARE;
+      return VideoCodecStatus.ERROR;
     }
 
     if (buffer.capacity() < size) {
       Logging.e(TAG, "decode() - HW buffer too small");
-      return VideoCodecStatus.FALLBACK_SOFTWARE;
+      return VideoCodecStatus.ERROR;
     }
     buffer.put(frame.buffer);
 
-    decodeStartTimes.offer(SystemClock.elapsedRealtime());
+    frameInfos.offer(new FrameInfo(SystemClock.elapsedRealtime(), frame.rotation));
     try {
       codec.queueInputBuffer(
           index, 0 /* offset */, size, frame.captureTimeMs * 1000, 0 /* flags */);
     } catch (IllegalStateException e) {
       Logging.e(TAG, "queueInputBuffer failed", e);
-      decodeStartTimes.pollLast();
-      return VideoCodecStatus.FALLBACK_SOFTWARE;
+      frameInfos.pollLast();
+      return VideoCodecStatus.ERROR;
     }
     if (keyFrameRequired) {
       keyFrameRequired = false;
@@ -278,7 +289,7 @@ class HardwareVideoDecoder implements VideoDecoder {
       codec = null;
       callback = null;
       outputThread = null;
-      decodeStartTimes.clear();
+      frameInfos.clear();
     }
     return VideoCodecStatus.OK;
   }
@@ -325,10 +336,12 @@ class HardwareVideoDecoder implements VideoDecoder {
         return;
       }
 
-      Long decodeStartTimeMs = decodeStartTimes.poll();
+      FrameInfo frameInfo = frameInfos.poll();
       Integer decodeTimeMs = null;
-      if (decodeStartTimeMs != null) {
-        decodeTimeMs = (int) (SystemClock.elapsedRealtime() - decodeStartTimeMs);
+      int rotation = 0;
+      if (frameInfo != null) {
+        decodeTimeMs = (int) (SystemClock.elapsedRealtime() - frameInfo.decodeStartTimeMs);
+        rotation = frameInfo.rotation;
       }
 
       hasDecodedFirstFrame = true;
@@ -374,8 +387,7 @@ class HardwareVideoDecoder implements VideoDecoder {
       codec.releaseOutputBuffer(result, false);
 
       long presentationTimeNs = info.presentationTimeUs * 1000;
-      VideoFrame frame =
-          new VideoFrame(frameBuffer, 0 /* rotation */, presentationTimeNs, new Matrix());
+      VideoFrame frame = new VideoFrame(frameBuffer, rotation, presentationTimeNs, new Matrix());
 
       // Note that qp is parsed on the C++ side.
       callback.onDecodedFrame(frame, decodeTimeMs, null /* qp */);
@@ -454,7 +466,7 @@ class HardwareVideoDecoder implements VideoDecoder {
     codec = null;
     callback = null;
     outputThread = null;
-    decodeStartTimes.clear();
+    frameInfos.clear();
     Logging.d(TAG, "Release on output thread done");
   }
 
@@ -492,14 +504,15 @@ class HardwareVideoDecoder implements VideoDecoder {
     copyPlane(src, vPos, uvStride, frameBuffer.getDataV(), 0, frameBuffer.getStrideV(), chromaWidth,
         chromaHeight);
 
-    // If the sliceHeight is odd, duplicate the last rows of chroma.
+    // If the sliceHeight is odd, duplicate the last rows of chroma.  Copy the last row of the U and
+    // V channels and append them at the end of each channel.
     if (sliceHeight % 2 != 0) {
       int strideU = frameBuffer.getStrideU();
-      uPos = chromaHeight * strideU;
-      copyRow(frameBuffer.getDataU(), uPos - strideU, frameBuffer.getDataU(), uPos, chromaWidth);
+      int endU = chromaHeight * strideU;
+      copyRow(frameBuffer.getDataU(), endU - strideU, frameBuffer.getDataU(), endU, chromaWidth);
       int strideV = frameBuffer.getStrideV();
-      vPos = chromaHeight * strideV;
-      copyRow(frameBuffer.getDataV(), vPos - strideV, frameBuffer.getDataV(), vPos, chromaWidth);
+      int endV = chromaHeight * strideV;
+      copyRow(frameBuffer.getDataV(), endV - strideV, frameBuffer.getDataV(), endV, chromaWidth);
     }
   }
 

webrtc/sdk/android/instrumentationtests/src/org/webrtc/HardwareVideoDecoderTest.java
@@ -67,6 +67,7 @@ public final class HardwareVideoDecoderTest {
     VideoDecoder decoder = decoderFactory.createDecoder(supportedCodecs[0].name);
 
     final long presentationTimestampUs = 20000;
+    final int rotation = 270;
 
     final CountDownLatch decodeDone = new CountDownLatch(1);
     final AtomicReference<VideoFrame> decoded = new AtomicReference<>();
@@ -98,7 +99,7 @@ public final class HardwareVideoDecoderTest {
     // First, encode a frame.
     VideoFrame.I420Buffer buffer = new I420BufferImpl(SETTINGS.width, SETTINGS.height);
     VideoFrame frame =
-        new VideoFrame(buffer, 0 /* rotation */, presentationTimestampUs * 1000, new Matrix());
+        new VideoFrame(buffer, rotation, presentationTimestampUs * 1000, new Matrix());
     VideoEncoder.EncodeInfo info = new VideoEncoder.EncodeInfo(
         new EncodedImage.FrameType[] {EncodedImage.FrameType.VideoFrameKey});
 
@@ -113,11 +114,11 @@ public final class HardwareVideoDecoderTest {
     ThreadUtils.awaitUninterruptibly(decodeDone);
 
     frame = decoded.get();
-    assertEquals(frame.getRotation(), 0);
+    assertEquals(frame.getRotation(), rotation);
     assertEquals(frame.getTimestampNs(), presentationTimestampUs * 1000);
     assertEquals(frame.getTransformMatrix(), new Matrix());
-    assertEquals(frame.getBuffer().getWidth(), SETTINGS.width);
-    assertEquals(frame.getBuffer().getHeight(), SETTINGS.height);
+    assertEquals(frame.getWidth(), SETTINGS.width);
+    assertEquals(frame.getHeight(), SETTINGS.height);
 
     assertEquals(decoder.release(), VideoCodecStatus.OK);
     assertEquals(encoder.release(), VideoCodecStatus.OK);